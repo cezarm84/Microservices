c7c9aa424a1b59c0bd6d78a890331811ed676d49
cezarm84 <cezarmh@gmail.com>
2024-03-02 20:44:22 +0100

    restructur;D

---
+++ uppgift/src/main/java/com/kaisar/uppgift/UppgiftApplication.java
@@ -0,0 +1,13 @@
+package com.kaisar.uppgift;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UppgiftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UppgiftApplication.class, args);
+	}
+
+}
---
+++ uppgift/src/test/java/com/kaisar/uppgift/UppgiftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kaisar.uppgift;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UppgiftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
