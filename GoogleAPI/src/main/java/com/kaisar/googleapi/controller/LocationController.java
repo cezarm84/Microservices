e332f5b49d1bbd7fc0187c101f20c6764eb1dd65
cezarm84 <cezarmh@gmail.com>
2024-03-03 20:23:10 +0100

    api key  api, and modify the uri

--- GoogleAPI/src/main/java/com/kaisar/googleapi/controller/LocationController.java
+++ GoogleAPI/src/main/java/com/kaisar/googleapi/controller/LocationController.java
@@ -1,38 +1,48 @@
 package com.kaisar.googleapi.controller;
 
 import com.kaisar.googleapi.model.Response;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.util.UriComponents;
-import org.springframework.web.util.UriComponentsBuilder;
 
 @RestController
 public class LocationController {
-    @Value("${API_KEY}")
-    private String API_KEY;
 
-    /* @GetMapping("/test")
-    public String test(){
-        return "test";
-    }*/
+    private static final Logger logger = LoggerFactory.getLogger(LocationController.class);
+
+    private final String apiKey;
+    private final RestTemplate restTemplate;
+
+    @Autowired
+    public LocationController(@Value("${API_KEY}") String apiKey, RestTemplate restTemplate) {
+        this.apiKey = apiKey;
+        this.restTemplate = restTemplate;
+    }
 
     @GetMapping("/locations")
-    public Response getTripDetails(@RequestParam String origin, @RequestParam String destination ) {
-        UriComponents uri = UriComponentsBuilder.newInstance()
-                .scheme("https")
-                .host("maps.googleapis.com")
-                .path("/maps/api/directions/json")
-                .queryParam("origin", origin)
-                .queryParam("destination",destination)
-                .queryParam("key",API_KEY)
-                .build();
-        ResponseEntity<Response> response =
-                new RestTemplate().getForEntity(uri.toUriString(),Response.class);
-        return response.getBody();
+    public Response getTripDetails() {
+        try {
+            ResponseEntity<Response> response = restTemplate.getForEntity(
+                    "https://maps.googleapis.com/maps/api/directions/json?origin=landvetter&destination=stockholm&key=" + apiKey,
+                    Response.class);
+
+            if (response.getStatusCode().is2xxSuccessful()) {
+                return response.getBody();
+            } else {
+                logger.error("Error in API response: {}", response.getStatusCode());
+                // Handle error appropriately or return a default response
+                return new Response();
+            }
+        } catch (Exception e) {
+            logger.error("Error while making API request", e);
+            // Handle exception appropriately or return a default response
+            return new Response();
+        }
     }
 }
 
